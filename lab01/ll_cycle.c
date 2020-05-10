b4c705cc0770347e33d57bc8e8eb4472cc4ae0ee
gichard <norepick@163.com>
2020-05-10 20:56:55 +0800

    Lab1--finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,22 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    int incTort = 0; // if incTort is 1, advance tortoise
+    while (hare) {
+	hare = hare->next;
+	if (incTort == 1) {
+            tortoise = tortoise->next;
+	    incTort = 0;
+	} else {
+            incTort = 1;
+	}
+
+	if (hare == tortoise) {
+            return 1;
+	}
+    }
+
     return 0;
 }
