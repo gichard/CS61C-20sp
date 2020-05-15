c735d81ac738f3a7c3243b5c58fee67d57e74a3c
norepick <norepick@163.com>
2020-05-15 23:54:49 +0800

    proj2B--finished read matrix

--- proj2/read_matrix.s
+++ proj2/read_matrix.s
@@ -19,17 +19,61 @@
 read_matrix:
 
     # Prologue
-	
+	addi sp, sp, -24
+	sw s4, 20(sp)
+	sw s3, 16(sp)
+	sw ra, 12(sp)
+	sw s0, 8(sp)
+	sw s1, 4(sp)
+	sw s2, 0(sp)
 
+	mv s0, a0
+	mv s1, a1
+	mv s2, a2
 
+	li a0, 8
+	jal ra, malloc
+	mv s3, a0 # buffer
+	mv a1, s0
+	li a2, 0
+	jal ra, fopen # open file
+	mv s4, a0 # file descriptor
+	mv a1, s4
+	mv a2, s3
+	li a3, 8
+	jal ra, fread # read first 8 bytes
+	li t0, 8
+	blt a0, t0, eof_or_error # handle error
 
+	lw t0, 0(s3) # rows
+	lw t1, 4(s3) # cols
+	sw t0, 0(s1)
+	sw t1, 0(s2)
+	mv s1, t0
+	mv s2, t1
+	mv a0, s3
+	jal ra, free # free old buffer
+	mul s0, s1, s2
+	slli s0, s0, 2 # calculate number of bytes needed
+	mv a0, s0
+	jal ra, malloc
+	mv s3, a0 # create new buffer for matrix
 
+	mv a1, s4
+	mv a2, s3
+	mv a3, s0
+	jal ra, fread
+	bne a0, s0, eof_or_error
 
-
-
-
-    # Epilogue
-
+	mv a0, s3
+	# Epilogue
+	lw s4, 20(sp)
+	lw s3, 16(sp)
+	lw ra, 12(sp)
+	lw s0, 8(sp)
+	lw s1, 4(sp)
+	lw s2, 0(sp)
+	addi sp, sp, 24
 
     ret
 
--- proj2/test_files/test_read_matrix.s
+++ proj2/test_files/test_read_matrix.s
@@ -7,10 +7,18 @@ file_path: .asciiz "./test_input.bin"
 .text
 main:
     # Read matrix into memory
-
+    li a0, 8
+	jal ra, malloc
+	mv s0, a0
+	mv a1, a0
+	addi a2, a0, 4 # set pointer to integer
+    la a0, file_path
+    jal ra, read_matrix
 
     # Print out elements of matrix
-
+    lw a1, 0(s0)
+    lw a2, 4(s0)
+    jal ra, print_int_array
 
     # Terminate the program
     addi a0, x0, 10
